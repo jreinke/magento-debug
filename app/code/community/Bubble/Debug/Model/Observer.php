102bba745966998ba3bcd3d2f220e443e319b4d6
Johann Reinke <johann.reinke@gmail.com>
2014-12-03 09:36:49 +0100

    Tag v1.0.2

--- app/code/community/Bubble/Debug/Model/Observer.php
+++ app/code/community/Bubble/Debug/Model/Observer.php
@@ -2,8 +2,8 @@
 /**
  * @category    Bubble
  * @package     Bubble_Debug
- * @version     1.0.1
- * @copyright   Copyright (c) 2013 BubbleCode (http://shop.bubblecode.net)
+ * @version     1.0.2
+ * @copyright   Copyright (c) 2014 BubbleShop (https://www.bubbleshop.net)
  */
 class Bubble_Debug_Model_Observer
 {
